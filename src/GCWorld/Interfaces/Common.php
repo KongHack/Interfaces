68421ad77de6ae9fddf4f059933182518ec13f46
Mitch <mitch.dodge@mhc.org>
2014-12-18 08:20:16 -0500

    Common & User Interfaces

---
+++ src/GCWorld/Interfaces/Common.php
@@ -0,0 +1,9 @@
+<?php
+namespace GCWorld\Interfaces;
+
+Interface Common
+{
+    public function DB();
+    public function getUser();
+    public function setUser();
+}
---
+++ src/GCWorld/Interfaces/User.php
@@ -0,0 +1,17 @@
+<?php
+namespace GCWorld\Interfaces;
+
+interface User
+{
+    public function sessCheck();
+    public function getVal();
+    public function getObject();
+    public function getID();
+
+    public function addMessage($message, $class='sitespecific');
+    public function printMessages();
+
+    public function getSetting($key);
+    public function setSetting($key, $value);
+
+}
