fd7ce0c021949a86e095eabcd9280da385787ec6
Mitch <mitch.dodge@mhc.org>
2014-12-18 09:50:13 -0500

    Form Interface

--- src/GCWorld/Interfaces/Form.php
+++ src/GCWorld/Interfaces/Form.php
@@ -3,7 +3,6 @@ namespace GCWorld\Interfaces;
 
 interface Form
 {
-    public function __construct();
     public function formDisplay();
-    public function formProcess();
+    public function formProcess($input);
 }
