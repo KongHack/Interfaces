1ce1078a350e9ac6febca010d65b4cac6b5857ab
Mitch <mitch.dodge@mhc.org>
2015-04-14 08:23:48 -0400

    Add Web Root & Doc Blocks

--- src/GCWorld/Interfaces/CommonConfig.php
+++ src/GCWorld/Interfaces/CommonConfig.php
@@ -3,7 +3,23 @@ namespace GCWorld\Interfaces;
 
 Interface CommonConfig
 {
+	/**
+	 * @return array
+	 */
 	public function configDB();
+
+	/**
+	 * @return string
+	 */
 	public function configRoot();
+
+	/**
+	 * @return string
+	 */
 	public function configDomain();
+
+	/**
+	 * @return string
+	 */
+	public function configWebRoot();
 }
