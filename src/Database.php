11f1dd7917eb2b66d596103a5be15d3d3e9ef39a
Mitch <mitch.dodge@mhc.org>
2015-08-04 10:09:58 -0400

    Updated Database Interface to match new functions

--- src/Database.php
+++ src/Database.php
@@ -5,4 +5,7 @@ Interface Database
 {
     public function ping();
     public function tableExists($table);
+    public function getWorkingDatabaseName();
+    public function getTableComment($table);
+    public function setTableComment($table, $comment);
 }
