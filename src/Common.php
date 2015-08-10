4bf37494394093cc8e8901c89d597215af2b0449
Mitch <mitch.dodge@mhc.org>
2015-08-10 09:05:38 -0400

    New Common Requirements

--- src/Common.php
+++ src/Common.php
@@ -3,7 +3,6 @@ namespace GCWorld\Interfaces;
 
 Interface Common
 {
-    public function DB();
-    public function getUser();
-    public function setUser();
+    public function getDatabase();
+    public function getCache();
 }
