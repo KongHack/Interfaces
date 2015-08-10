ee1590170617c2f96aea477d17fba3a3eb964b4b
Mitch <mitch.dodge@mhc.org>
2015-08-10 09:39:04 -0400

    Get Config function, doc blocks

--- src/Common.php
+++ src/Common.php
@@ -3,6 +3,19 @@ namespace GCWorld\Interfaces;
 
 Interface Common
 {
+    /**
+     * @return \GCWorld\Interfaces\Database
+     */
     public function getDatabase();
+
+    /**
+     * @return \Redis
+     */
     public function getCache();
+
+    /**
+     * @param $heading
+     * @return array
+     */
+    public function getConfig($heading);
 }
