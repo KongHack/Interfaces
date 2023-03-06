f98e7b3889636d51013bcecdaa719c73329c8d41
GameCharmer <1208565+GameCharmer@users.noreply.github.com>
2023-03-06 08:25:44 -0500

    ORMDescriptionInterface

---
+++ src/ORMDescriptionInterface.php
@@ -0,0 +1,26 @@
+<?php
+namespace GCWorld\Interfaces;
+
+/**
+ * Interface ORMDescriptionInterface
+ */
+interface ORMDescriptionInterface
+{
+    /**
+     * @param string $fieldName
+     * @return string|null
+     */
+    public static function getFieldTitle(string $fieldName): ?string;
+
+    /**
+     * @param string $fieldName
+     * @return string|null
+     */
+    public static function getFieldDesc(string $fieldName): ?string;
+
+    /**
+     * @param string $fieldName
+     * @return string|null
+     */
+    public static function getFieldHelp(string $fieldName): ?string;
+}
