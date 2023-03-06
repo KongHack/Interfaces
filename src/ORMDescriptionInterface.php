a7fabd516857a33abaa6efa10da693123adf5f83
GameCharmer <1208565+GameCharmer@users.noreply.github.com>
2023-03-06 09:33:12 -0500

    Update Interface to avoid conflicts

--- src/ORMDescriptionInterface.php
+++ src/ORMDescriptionInterface.php
@@ -10,23 +10,23 @@ interface ORMDescriptionInterface
      * @param string $fieldName
      * @return string|null
      */
-    public static function getFieldTitle(string $fieldName): ?string;
+    public static function getORMFieldTitle(string $fieldName): ?string;
 
     /**
      * @param string $fieldName
      * @return string|null
      */
-    public static function getFieldDesc(string $fieldName): ?string;
+    public static function getORMFieldDesc(string $fieldName): ?string;
 
     /**
      * @param string $fieldName
      * @return string|null
      */
-    public static function getFieldHelp(string $fieldName): ?string;
+    public static function getORMFieldHelp(string $fieldName): ?string;
 
     /**
      * @param string $fieldName
      * @return int
      */
-    public static function getFieldMaxLength(string $fieldName): int;
+    public static function getORMFieldMaxLength(string $fieldName): int;
 }
