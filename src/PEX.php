50a0b77302a3d04a81275aa56923c7677790d636
GameCharmer <1208565+GameCharmer@users.noreply.github.com>
2023-10-24 13:59:50 -0400

    Version Bump

--- src/PEX.php
+++ src/PEX.php
@@ -3,6 +3,14 @@ namespace GCWorld\Interfaces;
 
 interface PEX
 {
+    public const LEVEL_OVERRIDE = 3;
+    public const LEVEL_ALWAYS   = 2;
+    public const LEVEL_YES      = 1;
+    public const LEVEL_NA       = 0;
+    public const LEVEL_NO       = -1;
+    public const LEVEL_NEVER    = -2;
+    public const LEVEL_DISABLED = -3;
+
     /**
      * @param string $node
      * @return int|bool
